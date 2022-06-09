97151729efdf99fcc91337cf1250e1868e11deea
yusufekrem <yusufekremunlu@gmail.com>
2022-06-09 13:28:32 +0300

    First Commit

---
+++ app/src/main/java/com/yusufekrem/countries/view/CountryFragment.kt
@@ -0,0 +1,32 @@
+package com.yusufekrem.countries.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusufekrem.countries.R
+
+
+class CountryFragment : Fragment() {
+    private var countryUuid = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            countryUuid = CountryFragmentArgs.fromBundle(it).countryUUID
+        }
+    }
+}
---
+++ app/src/main/java/com/yusufekrem/countries/view/FeedFragment.kt
@@ -0,0 +1,40 @@
+package com.yusufekrem.countries.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusufekrem.countries.R
+
+class FeedFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Deneme testi için konuldu fragment calısıyor
+        /*
+        feedButton.setOnClickListener{
+            val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+        */
+
+    }
+
+
+
+
+
+}
